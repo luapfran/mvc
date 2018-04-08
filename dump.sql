a028072b9efe53235cf363da4083c84ec1fcc287
luapfran <luapfran@hotmail.com>
2018-04-08 00:18:19 -0300

    atualizado

--- dump.sql
+++ dump.sql
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 CREATE TABLE usuario
 (
    id INT PRIMARY KEY AUTO_INCREMENT,
    nome VARCHAR(100) NOT NULL,
    email VARCHAR(100) NOT NULL
-=======
-CREATE TABLE usuario
-(
-   id INT PRIMARY KEY AUTO_INCREMENT,
-   nome VARCHAR(100) NOT NULL,
-   email VARCHAR(100) NOT NULL
->>>>>>> 13a56c9b89ef6fe2a30f7acdd89dcdd09ad19f1b
 );
